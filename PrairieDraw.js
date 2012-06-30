986142eec9739907b4ea7ec131c4eb96a4d82802
Matthew West <mwest@illinois.edu>
2012-06-30 10:56:40 -0500

    bugfix: draw arrows with correct width

--- PrairieDraw.js
+++ PrairieDraw.js
@@ -496,29 +496,35 @@ PrairieDraw.prototype._arrowhead = function(posDw, dirDw, lenPx) {
     @param {string} type Optional type of vector being drawn.
 */
 PrairieDraw.prototype.arrow = function(startDw, endDw, type) {
-    var startPx = this.pos2Px(startDw);
-    var endPx = this.pos2Px(endDw);
+    var offsetDw = endDw.subtract(startDw);
+    var offsetPx = this.vec2Px(offsetDw);
+    var arrowLengthPx = offsetPx.modulus();
+    var lineEndDw, drawArrowHead, arrowheadLengthPx;
+    if (arrowLengthPx < 1) {
+        // if too short, just draw a simple line
+        lineEndDw = endDw;
+        drawArrowHead = false;
+    } else {
+        var arrowheadMaxLengthPx = this._props.arrowheadLengthRatio * this._props.arrowLineWidthPx;
+        arrowheadLengthPx = Math.min(arrowheadMaxLengthPx, arrowLengthPx / 2);
+        var arrowheadCenterLengthPx = (1 - this._props.arrowheadOffsetRatio) * arrowheadLengthPx;
+        var lineLengthPx = arrowLengthPx - arrowheadCenterLengthPx;
+        lineEndDw = startDw.add(offsetDw.x(lineLengthPx / arrowLengthPx));
+        drawArrowHead = true;
+    }
 
+    var startPx = this.pos2Px(startDw);
+    var lineEndPx = this.pos2Px(lineEndDw);
     this.save();
     this._ctx.lineWidth = this._props.arrowLineWidthPx;
     this._setLineStyles(type);
-
-    var offsetDw = endDw.subtract(startDw);
-    var offsetPx = endPx.subtract(startPx);
-    var arrowLengthPx = offsetPx.modulus();
-    if (arrowLengthPx < 1) {
-        // if too short, just draw a simple line
-        this.line(startDw, endDw);
-        return;
+    this._ctx.beginPath();
+    this._ctx.moveTo(startPx.e(1), startPx.e(2));
+    this._ctx.lineTo(lineEndPx.e(1), lineEndPx.e(2));
+    this._ctx.stroke();
+    if (drawArrowHead) {
+        this._arrowhead(endDw, offsetDw, arrowheadLengthPx);
     }
-
-    var arrowheadMaxLengthPx = this._props.arrowheadLengthRatio * this._props.arrowLineWidthPx;
-    var arrowheadLengthPx = Math.min(arrowheadMaxLengthPx, arrowLengthPx / 2);
-    var arrowheadCenterLengthPx = (1 - this._props.arrowheadOffsetRatio) * arrowheadLengthPx;
-    var lineLengthPx = arrowLengthPx - arrowheadCenterLengthPx;
-    var lineEndDw = startDw.add(offsetDw.x(lineLengthPx / arrowLengthPx));
-    this.line(startDw, lineEndDw, type);
-    this._arrowhead(endDw, offsetDw, arrowheadLengthPx);
     this.restore();
 }
 
